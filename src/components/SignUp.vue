b79736f7dde0320e56c5a365a8c760c5151ab6ff
Flávio Mota <fmota@student.42lisboa.com>
2023-02-27 12:30:19 +0000

    setting my api to collect user sign up data

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -2,16 +2,41 @@
 	<img class="logo" src="../assets/rest-logo.png" />
 	<h1>Sign Up</h1>
 	<div class="register">
-		<input type="text" placeholder="Enter Name" />
-		<input type="text" placeholder="Enter Email" />
-		<input type="password" placeholder="Enter Password" />
-		<button>Sign Up</button>
+		<input type="text" v-model="name" placeholder="Enter Name" />
+		<input type="text" v-model="email" placeholder="Enter Email" />
+		<input type="password" v-model="password" placeholder="Enter Password" />
+		<button v-on:click="signUp">Sign Up</button>
 	</div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
 	name: 'SignUp',
+	data() {
+		return {
+			name: '',
+			email: '',
+			password: ''
+		}
+	},
+	methods: {
+		async signUp(){
+			let result = await axios.post("http://localhost:3000/users", {
+				name: this.name,
+				email: this.email,
+				password: this.password
+			});
+
+			console.warn(result);
+
+			if(result.status == 201) {
+				alert("Sign Up Successfully!");
+				localStorage.setItem("user-info", JSON.stringify(result.data));
+			}
+		}
+	}
 }
 </script>
 
