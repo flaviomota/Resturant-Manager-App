db354a1078bf5eacef76ec82750da5044f865595
Flávio Mota <fmota@student.42lisboa.com>
2023-02-27 11:27:09 +0000

    sign up design

---
+++ src/components/SignUp.vue
@@ -0,0 +1,43 @@
+<template>
+	<img class="logo" src="../assets/rest-logo.png" />
+	<h1>Sign Up</h1>
+	<div class="register">
+		<input type="text" placeholder="Enter Name" />
+		<input type="text" placeholder="Enter Email" />
+		<input type="password" placeholder="Enter Password" />
+		<button>Sign Up</button>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'SignUp',
+}
+</script>
+
+<style>
+.logo {
+	width: 100px;
+	border-radius: 50px;
+}
+
+.register input {
+	width: 300px;
+	height: 40px;
+	padding-left: 20px;
+	display: block;
+	margin-bottom: 30px;
+	margin-right: auto;
+	margin-left: auto;
+	border: 1px solid skyblue;
+}
+
+.register button {
+	width: 320px;
+	height: 40px;
+	border: 1px solid skyblue;
+	background-color: skyblue;
+	color: #fff;
+	cursor: pointer;
+}
+</style>
